1c2a249097acf0f770446c5ac6ac44d9390ce6fc
SantiagoGuty <santiago.gutierrezmorales@valpo.edu>
2026-03-09 12:24:36 -0400

    SEO metadata implementation pt2 robots & sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    { url: "https://clubhousevianey.com", lastModified: new Date() },
+    { url: "https://clubhousevianey.com/servicios", lastModified: new Date() },
+    { url: "https://clubhousevianey.com/quienes-somos", lastModified: new Date() },
+    { url: "https://clubhousevianey.com/contactanos", lastModified: new Date() },
+  ];
+}
